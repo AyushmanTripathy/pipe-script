646e498db6bf8f773fd43065e0d9998071b3a3b5
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2021-10-02 14:16:45 +0530

    removed comilation idea

---
+++ src/interpreter.js
@@ -0,0 +1,60 @@
+import runGlobalScope from "./execution.js";
+import classifyScopes from "./process.js";
+
+import { createInterface } from "readline";
+import { readFileSync, existsSync, createReadStream } from "fs";
+
+const args = process.argv.splice(2);
+const cwd = process.cwd();
+
+globalThis.config = loadJson("../config.json");
+
+function loadJson(path) {
+  const content = readFileSync(new URL(path, import.meta.url));
+  return JSON.parse(content);
+}
+
+globalThis.log = (string) => {
+  console.log(string);
+};
+
+globalThis.error = (msg) => {
+  console.log(`[ERROR] ${msg}`);
+  process.exit();
+};
+
+init()
+function init() {
+  run([`import ${args.shift()}`]);
+}
+
+async function run(file) {
+  globalThis.scopes = {};
+  globalThis.hash_code = 0;
+  scopes.global = [];
+
+  scopes.vars = {};
+  scopes.object = {};
+  scopes.array = {};
+  scopes.string = {}
+  
+  await classifyScopes(file, importFile);
+  runGlobalScope();
+  if (typeof release_mode == "undefined") console.log(scopes);
+}
+
+async function importFile(path) {
+  const path_to_file = cwd + "/" + path.trim();
+  if (!existsSync(path_to_file))
+    return error(`path: ${path_to_file} doesnot exist`);
+
+  const fileStream = createReadStream(path_to_file);
+
+  const rl = createInterface({
+    input: fileStream,
+  });
+
+  const file = [];
+  for await (const line of rl) file.push(line);
+  await classifyScopes(file, importFile);
+}
