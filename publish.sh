1aaeb38ed0ba7c27209ea4ed012519f652820d9c
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2021-10-17 12:10:02 +0530

    publishing

--- publish.sh
+++ publish.sh
@@ -28,15 +28,7 @@ npm publish
 # pushing to master
 git push origin master
 
-# merging release branch
-echo "merging master --> release"
-curl \
-  -X POST \
-  -u "AyushmanTripathy":$(cat ~/.pat) \
-  -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/AyushmanTripathy/pipe-script/merges" \
-  -d '{"base":"release","head":"master"}'
-echo "merge complete"
+merge-master-release
 
 git commit -am "published $(npm view psre version)"
 echo 'successfully published'
